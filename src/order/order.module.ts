3ac44988ea8723617d1b98211867902ed23b12d9
Ammar321 <ammarnadeem1234@gmail.com>
2025-06-27 20:12:52 +0500

    dependency Fix

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,9 +6,10 @@ import { OrderEntity } from './entities/order.entity';
 import { OrderItemEntity } from './entities/order-item.entity';
 import { ProductEntity } from '../food-item/entities/product.entity';
 import { User } from '../user/entities/user-entity';
+import { FoodItemEntity } from 'src/food-item/entities/food-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity, OrderItemEntity, ProductEntity, User])],
+  imports: [TypeOrmModule.forFeature([OrderEntity, OrderItemEntity, ProductEntity, User, FoodItemEntity])],
   controllers: [OrderController],
   providers: [OrderService],
 })
