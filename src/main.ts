23ef701465876a806d04facf8c95e4ff6c45f5b8
Ammar321 <ammarnadeem1234@gmail.com>
2025-07-03 03:24:19 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { ClassSerializerInterceptor } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 
 async function bootstrap() {
@@ -8,15 +8,6 @@ async function bootstrap() {
 
   // 👇 Register global ClassSerializerInterceptor to handle @Exclude()
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
-  // 👇 Register global ValidationPipe to handle arrays and DTO validation
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    })
-  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
