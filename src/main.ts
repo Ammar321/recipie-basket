27c40ba62ab28918acd9b6ca46e1b0a52e9a376c
Ammar321 <ammarnadeem1234@gmail.com>
2025-07-03 04:03:09 +0500

    enabling cores

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { Reflector } from "@nestjs/core";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'https://running-frontend.vercel.app', // React dev server
+    credentials: true,              // if you're using cookies or auth headers
+  });
   // 👇 Register global ClassSerializerInterceptor to handle @Exclude()
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
