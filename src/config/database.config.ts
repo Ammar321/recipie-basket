b99a9741eb0d24a2516add7c3b96f124ceba5d3f
Ammar321 <ammarnadeem1234@gmail.com>
2025-07-03 01:32:39 +0500

    db fix

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,8 +9,8 @@ import { OrderItemEntity } from "src/order/entities/order-item.entity";
 
 export default registerAs("database", () => ({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT ?? "5432", 10),
+  host: process.env.DB_HOST || "switchback.proxy.rlwy.net",
+  port: parseInt(process.env.DB_PORT ?? "8080", 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
